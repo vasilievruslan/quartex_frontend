3fbd80bf484272fcdc12bc365e10e4f299cdec91
Ruzerio <ruzero.io@gmail.com>
2018-03-16 20:37:14 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+//= slick.js
+
+$(function () {
+	$('.carousel').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		responsive: [
+	    {
+	      breakpoint: 824,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2,
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }]
+	});
+})
